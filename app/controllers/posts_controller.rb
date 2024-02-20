0a60bee37c4a3af4437fb96323368782832a5984
Keitayas2 <keita.y2004@gmail.com>
2024-02-20 16:21:33 +0900

    make edit show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new(post_params)
+    post.user_id = current_user.id
     if post.save!
       redirect_to root_path
     else
@@ -18,12 +19,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post)
+    else
+      redirect_to edit_post_path(post)
+    end
   end
 
   private
   def post_params
     params.require(:post).permit(:body)
   end
+
 end
